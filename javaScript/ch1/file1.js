d3e655a0e687cb6f066052a88e24230e5cdb44c6
Nikhil Singh <nikhilsingh95969@gmail.com>
2022-12-06 22:45:18 +0530

    tailwind css

---
+++ javaScript/ch1/file1.js
@@ -0,0 +1,13 @@
+
+ const main = document.querySelector("#content");
+
+const cont = `
+    <div class="cont">
+        <h1>Nikhil Singh</h1>
+        
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem cum deserunt est.</p>
+
+        <a href="#">Google</a>
+    </div>
+`
+main.innerHTML = cont;
